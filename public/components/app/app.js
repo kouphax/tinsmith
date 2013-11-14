bd5276e72e34207592f9756eff0dbb7ac6f1500a
James Hughes <james@yobriefca.se>
2013-11-14 13:41:57 +0000

    [0.0.6] Evaluate is no longer manual use Ctrl-Enter or Cmd-Enter, Added console info message, refactored javascript

--- public/components/app/app.js
+++ public/components/app/app.js
@@ -1,71 +1,85 @@
 (function(undefined) {
-    function debounce(fn, delay) {
-        var timer = null;
-        return function () {
-            var context = this, args = arguments;
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                fn.apply(context, args);
-            }, delay);
-        };
+    function evaluate(force) {
+        var value = editor.getValue();
+        if(force === true || encodeURI(value) != window.location.search.substring(1)) {
+            outputs.style.opacity = "0.75"
+
+            if (history.pushState) {
+                var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + encodeURI(value);
+                window.history.pushState({path:newurl},'',newurl);
+            }
+
+            ws.send(value)
+        }
     }
 
-    var outputPane = document.getElementById("outputs");
-    var editor = CodeMirror(document.getElementById("code"), {
-        mode:  "text/x-scala",
-        theme: "solarized light",
-        smartIndent: false,
-        tabSize: 2,
-        lineNumbers: true
-    });
+    function maskOutputs() {
+        outputs.style.opacity = "0.75";
+    }
 
-    var display = CodeMirror(document.getElementById("output"), {
-        mode:  "text/x-scala",
-        theme: "solarized light",
-        readOnly: true,
-        tabSize: 2,
-        lineNumbers: true
-    })
+    function unmaskOutputs() {
+        outputs.style.opacity = "1";
+    }
 
-    var console = CodeMirror(document.getElementById("console"), {
+    function codeMirrorSettings(ext) {
+        ext = ext || {}
+        var merged = {};
+        var defaults = {
+            theme: "solarized light",
+            smartIndent: false,
+            tabSize: 2,
+            lineNumbers: true
+        };
+
+        for (var prop in defaults) { merged[prop] = defaults[prop]; }
+        for (var prop in ext) { merged[prop] = ext[prop]; }
+
+        return merged
+    }
+
+    var editor = CodeMirror(document.getElementById("code"), codeMirrorSettings({
         mode:  "text/x-scala",
-        theme: "solarized light",
-        readOnly: true,
-        tabSize: 2,
-        lineNumbers: true,
+        extraKeys: {
+            "Alt-Enter": evaluate,
+            "Cmd-Enter": evaluate
+        }
+    }));
+
+    var display = CodeMirror(document.getElementById("output"), codeMirrorSettings())
+
+    var console = CodeMirror(document.getElementById("console"), codeMirrorSettings({
+        value: "Welcome to Tinsmith.\nYou are standing in an open field.\n\nAlt-Enter or ⌘-Enter to evaluate",
         lineNumberFormatter: function(){
             return ">"
         }
-    })
-
-    editor.on("change", debounce(function(me, obj){
-        outputs.style.opacity = "0.75"
-
-        var value = me.getValue();
-
-        if (history.pushState) {
-            var newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + encodeURI(value);
-            window.history.pushState({path:newurl},'',newurl);
-        }
-        ws.send(value)
-    }, 1500))
+    }));
 
     var ws = new WebSocket('ws://' + window.location.host + '/stream')
+
     ws.onmessage = function( message ) {
-        outputs.style.opacity = "1"
+        unmaskOutputs();
         var data = JSON.parse(message.data);
         display.setValue(data.userRepr);
         console.setValue(data.output);
     };
 
     ws.onerror = function(){
-        outputs.style.opacity = "1";
+        unmaskOutputs();
         display.setValue("error");
         editor.log(arguments);
     }
 
+
     if(window.location.search !== "") {
-        outputs.style.opacity = "0.75";
+        maskOutputs();
         editor.setValue(decodeURI(window.location.search.substring(1)));
+
+        if(ws.readyState == 1) {
+            evaluate(true);
+        }else {
+            ws.onopen = function() {
+                evaluate(true);
+            }
+        }
     }
 })()
